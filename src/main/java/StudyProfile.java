c287f94faf37cf8dedab6ed44d16fa73df8578fc
smvexchange <smvexchange@gmail.com>
2022-08-18 17:36:40 +0300

    Comlete task 24.7

---
+++ src/main/java/StudyProfile.java
@@ -0,0 +1,10 @@
+public enum StudyProfile {
+    PROGRAMMING("Программирование"),
+    JOURNALISM("Журналистика"),
+    HISTORY("История"),
+    MANAGEMENT("Управление"),
+    JURISPRUDENCE("Юриспруденция");
+
+    StudyProfile(String profileName) {
+    }
+}
---
+++ src/main/java/University.java
@@ -0,0 +1,77 @@
+public class University {
+    private String id;
+    private String fullName;
+    private String shortName;
+    private int yearOfFoundation;
+    private StudyProfile mainProfile;
+
+    public University() {
+        this.id = "#1";
+        this.fullName = "Lomonosov Moscow State University ";
+        this.shortName = "MSU";
+        this.yearOfFoundation = 1755;
+        this.mainProfile = StudyProfile.JOURNALISM;
+    }
+
+    public University(String id, String fullName, String shortName, int yearOfFoundation, StudyProfile mainProfile) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.yearOfFoundation = yearOfFoundation;
+        this.mainProfile = mainProfile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public University setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public University setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public University setShortName(String shortName) {
+        this.shortName = shortName;
+        return this;
+    }
+
+    public int getYearOfFoundation() {
+        return yearOfFoundation;
+    }
+
+    public University setYearOfFoundation(int yearOfFoundation) {
+        this.yearOfFoundation = yearOfFoundation;
+        return this;
+    }
+
+    public StudyProfile getMainProfile() {
+        return mainProfile;
+    }
+
+    public University setMainProfile(StudyProfile mainProfile) {
+        this.mainProfile = mainProfile;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " "
+                + this.fullName + "("
+                + this.shortName + "), year of foundation: "
+                + this.yearOfFoundation + ", main profile: "
+                + this.mainProfile + ".";
+    }
+}
